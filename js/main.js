22fa8afb21385e0181e02dadb461cfdaf41e7df8
Prateek Dubey <prateeklndubey@gmail.com>
2018-01-13 15:12:47 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@
 
   var active = 0,
       elems = document.getElementsByClassName("projectContent"),
-      titleText = ["Word Scrapper", "Portfolio","Scrapper Running"],
+      titleText = ["Word Scrapper", "Portfolio","Scrapper Running","SRK Tribute Page"],
       activeTab = document.getElementsByClassName("active")[0];
 
   function next(){
