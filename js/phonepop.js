d2192be5dc9435a15ea57872f9c9116021a78791
antonio-erick <antonioericksilvapereira@gmail.com>
2022-02-12 09:42:09 -0300

    Fixed typos and polished the code

---
+++ js/phonepop.js
@@ -0,0 +1,9 @@
+function phonePop() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+};
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-8P65YX5PJ3');
